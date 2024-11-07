4ca9bd16c743c524db3c59b3f1fe801acb52d622
AceS0 <abse0001@stud.kea.dk>
2024-11-07 10:24:52 +0100

    Added extended functionalities

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,29 @@ public class Main {
         for (Covid19Data data : datas) {
             System.out.println(data);
         }
+
+
+        comparator = new RegionComparator().thenComparing(new AldersGruppeComparator());
+        comparator2 = new AldersGruppeComparator().thenComparing(new RegionComparator());
+        System.out.println("Choose betweeen primary Region & secondary Age or primary Age & secondary Region," +
+                " by typing 1 for Region and 2 for Age.");
+        while(true) {
+            String number = sc.next();
+            if (Objects.equals(number, "1")) {
+                Collections.sort(datas, comparator);
+                break;
+            } else if (Objects.equals(number, "2")) {
+                Collections.sort(datas, comparator2);
+                break;
+            } else{
+                System.out.println("The input doesn't match.");
+            }
+        }
+        System.out.println("Data has been changed.");
+
+        for (Covid19Data data : datas) {
+            System.out.println(data);
+        }
+
     }
 }
